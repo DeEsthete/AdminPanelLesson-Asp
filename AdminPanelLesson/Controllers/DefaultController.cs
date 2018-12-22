4f5a0d4993be8bccfa832e2d1301f0dbc9afadd5
JekaLion <djoni-01@mail.ru>
2018-12-22 13:57:35 +0600

    init

---
+++ AdminPanelLesson/Controllers/DefaultController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AdminPanelLesson.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public async Task<ActionResult> Index()
+        {
+            return File(@"C:\Users\ПавловЕ\Downloads\parallax.js-1.5.0.zip", "application/zip");
+        }
+    }
+}
